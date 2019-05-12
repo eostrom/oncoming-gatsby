c1e464f82760ba82a2948f5aefb0730cfc03f9c8
Erik Ostrom <erik@echographia.com>
2019-05-11 23:39:46 -0500

    Moved member list into Markdown files.

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,28 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `people`,
+        path: `${__dirname}/src/people`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: []
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`
       }
---
+++ src/people/bryce-kalal.md
@@ -0,0 +1,5 @@
+---
+name: Bryce Kalal
+picture: ''
+
+---
