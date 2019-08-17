c6118e9f94479765a14b99584bebf4e491e9387b
Erik Ostrom <erik@echographia.com>
2019-08-17 15:02:18 -0500

    New current show: Geminae 2019.

--- src/components/currentShow.js
+++ src/components/currentShow.js
@@ -6,7 +6,7 @@ const CurrentShow = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: {eq: "FiveFifths-2019-L.png"}) {
+        placeholderImage: file(relativePath: {eq: "geminae-2018.jpg"}) {
           childImageSharp {
             fluid(maxWidth: 820, traceSVG: {blackOnWhite: false}) {
               ...GatsbyImageSharpFluid_tracedSVG
@@ -23,49 +23,45 @@ const CurrentShow = () => (
           alt=""
         />
 
-        <h2>Five Fifths of Mary Poppins</h2>
+        <h2>
+          Geminae
+          <span class="subhead"> at the Twin Cities Horror Festival</span>
+        </h2>
 
         <p>
           <b>
-            Celebrate 26 Supercalifragilisticexpialidocious Years of Minnesota
-            Fringe!
+            The 2018 MN Fringe Festival horror hit is back, new and improved!
           </b>{' '}
-          Join us as five adventurous companies donate their time and talent to
-          deliver their spoonful of sugar take on a timeless classic. Featuring
-          performances from: Shrieking Harpies, Sheep Theater, Oncoming
-          Productions, Javier Morillo, and ALL DAY.
+          A routine mission goes awry when astronaut Cassie Matheson
+          sees something impossible in her shuttle. At mission control,
+          her sister Helen fights to bring her home. When the unthinkable
+          happens, they learn just how unbreakable the bond between them is,
+          with chilling consequences.
         </p>
 
         <div className="show-info">
           <div className="show-info-item">
-            <strong>Date:</strong> May 6, 2019
+            <strong>Dates:</strong> October 24th to November 3rd, 2019
           </div>
 
           <div className="show-info-item">
-            <strong>Time:</strong> Doors open at 6:30pm / Show starts at 7:30pm
+            <strong>Place:</strong>{' '}
+            <a href="https://southerntheater.org">The Southern Theatre,</a>
+            {' '}1420 S. Washington Ave.,
+            Minneapolis, MN 55454
           </div>
 
           <div className="show-info-item">
-            <strong>Place:</strong> Park Square Theatre, 20 West 7th Place,
-            Saint Paul, MN 55102
-          </div>
-
-          <div className="show-info-item">
-            <strong>Tickets:</strong> $45 in advance / $55 at the door / Free
-            for{' '}
-            <a href="https://www.minnesotafringe.org/members">
-              Fringe with Benefits
-            </a>{' '}
-            Members
+            <strong>Tickets:</strong> Festival passes available now
           </div>
         </div>
 
         <p>
           <a
             className="button"
-            href="https://www.minnesotafringe.org/BookingRetrieve.aspx?ID=274659"
+            href="http://www.tchorrorfestival.com/lineup-and-tickets1.html"
           >
-            BUY TICKETS
+            BUY A PASS
           </a>
         </p>
       </section>
