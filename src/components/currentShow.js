d6a83e8a11c3b5bcab63955c568a044fd1d2a0b2
Erik Ostrom <code@erikostrom.com>
2021-03-14 16:25:27 -0500

    Dead North update.

--- src/components/currentShow.js
+++ src/components/currentShow.js
@@ -6,7 +6,7 @@ const CurrentShow = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: {eq: "BlightChristmas_site.png"}) {
+        placeholderImage: file(relativePath: {eq: "DeadNorth_banner.png"}) {
           childImageSharp {
             fluid(maxWidth: 820, traceSVG: {blackOnWhite: false}) {
               ...GatsbyImageSharpFluid_tracedSVG
@@ -23,60 +23,35 @@ const CurrentShow = () => (
           alt=""
         />
 
-        <h2>
-          <span>Blight Christmas</span>
-          <a className="button" href="https://paypal.me/pools/c/8vIP67G5gq">
-            DONATE
-          </a>
-          {/*<span className="subhead"> at the Twin Cities Horror Festival</span>*/}
-        </h2>
+        <h2>Dead North</h2>
 
         <p>
-          Just because we’re all apart this holiday, that doesn’t mean we can’t
-          get scared together. Welcome to Blight Christmas: A Horror Holiday
-          Together… a spooky yet festive variety show for your ears, featuring
-          drama, prose, verse, and song from some of the finest creators in the
-          Twin Cities horror theater community and beyond.
+          Dead North, produced by Oncoming Productions and Hot Chocolate Media,
+          is horror theater for your ears - lovingly crafted aural chills by
+          some of the finest dark-theater makers from the Upper Midwest and
+          beyond.
         </p>
 
-        <p>Featuring work by:</p>
+        <p>
+          Listen now at{' '}
+          <a href="https://www.hotchocolatemedia.net/dead-north">
+            Hot Chocolate Media
+          </a>{' '}
+          or wherever you find your podcasts.
+        </p>
 
-        <ul>
-          <li>Amnesia Wars Productions</li>
-          <li>Big Spooky Radio Christmastime</li>
-          <li>The Champagne Drops</li>
-          <li>Eric Thompson</li>
-          <li>LandmanLand</li>
-          <li>Market Garden Theater</li>
-          <li>Oncoming Productions</li>
-          <li>Reverend Matt’s Monster Science</li>
-          <li>Sad Holiday</li>
-          <li>Special When Lit</li>
-          <li>Tom Reed</li>
-          <li>The Winding Sheet Outfit</li>
-        </ul>
-
-        <p>and produced in collaboration with Hot Chocolate Media.</p>
-
-        <div className="show-info">
-          <div className="show-info-item">
-            <strong>LISTEN NOW:</strong>{' '}
-            <a href="http://www.hotchocolatemedia.net/blight-christmas">
-              http://www.hotchocolatemedia.net/blight-christmas
+        <ul className="actions">
+          <li>
+            <a className="button" href="https://paypal.me/pools/c/8xIL7PIg12">
+              one-time donation
             </a>
-          </div>
-        </div>
-
-        <p>
-          <a className="button" href="https://paypal.me/pools/c/8vIP67G5gq">
-            DONATE
-          </a>
-        </p>
-
-        <p>
-          All donations will be shared among the contributing companies to
-          support their continued work.
-        </p>
+          </li>
+          <li>
+            <a className="button" href="https://www.patreon.com/deadnorth">
+              become a supporter
+            </a>
+          </li>
+        </ul>
       </section>
     )}
   />
